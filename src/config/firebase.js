9a0e68d6db159a0d4c505c8e3e276c473d63891f
PonceRojasRoger <rogerponce761@gmail.com>
2026-02-18 09:45:47 -0400

    feat: update Firebase config to radiolinea-app project

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,18 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBoqgldEzlobMPNuvFU_HU-lGt7tYpbaK8",
-  authDomain: "mav2025-bd18e.firebaseapp.com",
-  databaseURL: "https://mav2025-bd18e-default-rtdb.firebaseio.com",
-  projectId: "mav2025-bd18e",
-  storageBucket: "mav2025-bd18e.firebasestorage.app",
-  messagingSenderId: "439845573858",
-  appId: "1:439845573858:web:2f0a76db59be59c9e33abb",
-  measurementId: "G-CP0WCTVQWF",
+  apiKey: "AIzaSyC9xF4UX5i-VTtjjDEGG0HBXyIYqMp4Dl4",
+  authDomain: "radiolinea-app.firebaseapp.com",
+  databaseURL: "https://radiolinea-app-default-rtdb.firebaseio.com",
+  projectId: "radiolinea-app",
+  storageBucket: "radiolinea-app.firebasestorage.app",
+  messagingSenderId: "34355687130",
+  appId: "1:34355687130:web:9e02f6e8bbdd9f20867fe3",
+  measurementId: "G-TQ3XSTDYZ9",
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
